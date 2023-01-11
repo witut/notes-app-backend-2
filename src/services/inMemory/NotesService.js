5f7990ecbcf904d9c411d1f7caae694ddc41f422
Gusti Suparta <gusti.tut@gmail.com>
2023-01-11 12:49:22 +0800

    mengubah pesan pada throw error menjadi catatan tidak ditemukan

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -58,7 +58,7 @@ class NotesService {
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
     if (index === -1) {
-      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+      throw new Error('Catatan tidak ditemukan');
     }
     this._notes.splice(index, 1);
   }
